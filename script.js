3550e320b9a719ff253b615041a7e98cc542f217
Rajeev Kumar Kaushal <165012448+KaushalRaj2@users.noreply.github.com>
2026-09-05 04:20:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function makeid(l) {
-  // write your code here
+	let result=""
+	let rand="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
+	for(let i=0; i<l;i++){
+		result+=rand.charAt(Math.floor(Math.random() * rand.length) + 0)
+	}
+	return result;
 }
 
 // Do not change the code below.
